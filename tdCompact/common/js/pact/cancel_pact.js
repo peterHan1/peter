891e23b152b6f3a9aa60cb36c812f6373880279a
bo <bo>
2017-07-18 19:04:26 +0800

    7.18

---
+++ tdCompact/common/js/pact/cancel_pact.js
@@ -0,0 +1,22 @@
+$(function(){
+		$(".keyword").on('keydown',function(e) {  
+            var keycode = e.keyCode;  
+            var searchName = $(this).val();  
+            if(keycode=='13') {  
+            	alert("你按下了搜索键" + keycode)
+                e.preventDefault();    
+                //请求搜索接口    
+                 // 搜索为空
+                $(".opa_d,section").hide();
+               	$(".search_no").show();
+               	
+				// 搜素成功
+                $(".opa_d,section").hide();
+                $(".search_show").show();
+                var data_li = '<li><div class="cont_top"><p><span>20160606001</span> <span class="font_ora fr">存在法律隐患，条款变更</span></p><p class="p_cont"><span class="fl">张三</span><span>18866668888</span><span class="fr">浙A521N0</span></p></div><div class="cont_bot"><p>2017-04-01  11:45:30</p></div></li>';
+				$(".search_data").append(data_li);
+            }  
+     	});  
+     	
+	})
+	
---
+++ tdCompact/common/js/search.js
@@ -0,0 +1,31 @@
+$(".keyword").on("click",function(){
+	var h = $("#left_ment").height();
+	var ish = $(".keyword").height();
+	$(".search_d").width('85%');
+	$(".search").css("backgroundColor","rgba(0,0,0,0.3)");
+	$(".top").hide();
+	$(".search,.sea_can,.opa_d").show();
+	$(".search_show").height(h-ish);
+
+});
+$('.keyword').bind('input propertychange', function() {  
+    if($(this).val() != ""){
+    	$(".search_x").show();
+    }else{
+    	$(".search_x").hide();
+    	
+    }
+});  
+$(".search_x").on("click",function(){
+	$('.keyword').val("").focus();
+    $(".search_x").hide();
+})
+$(".sea_can").on("click",function(){
+	$(".search_d").width('95%');
+	$(".top,section").show();
+	$(".search").css("backgroundColor","#e2e3e7");
+	
+	$(".search_show,.sea_can,.opa_d,.search_x,.search_no").hide();
+	$(".search input").val("");
+	$(".search_data").html("");
+});
