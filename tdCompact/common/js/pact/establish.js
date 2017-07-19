541f95ba34954499b6bcdc5c2ec3677a15349a33
bo <bo>
2017-07-19 18:22:49 +0800

    7.19

--- tdCompact/common/js/pact/establish.js
+++ tdCompact/common/js/pact/establish.js
@@ -25,13 +25,53 @@ $(function(){
 	$(".est_contR").on("click",function(){
  		layer.open({
 			type: 1, 
-			title:"6666",
+			title:"",
 			content:$('#can_show'),
+			scrollbar: false,
 			closeBtn: 0,
-			area: ['50%', '20%']
+			area: ['5.8rem', 'auto']
 		}); 
     })
-	$("#remark_reset").on("click",function(){
+		var txts=$("#inp_txt").val().trim();
+			var length=txts.length;
+			$("#inp_txt").keyup(function(){//输入字判断字数
+		  		fontNum('inp_txt','lengthnums');
+		});
+		
+		function fontNum(idval,idnum){//判断字数
+			var a = $('#'+idval);
+			var b = $('#'+idnum);
+			var lennum = a.val().length;
+			b.html(lennum);	
+			if(lennum >= 1 && lennum <= 20){
+				$(".sub_btn").addClass("obs");
+				$("#inp_txt").removeClass("orange");
+				$("#lengthnums").removeClass("orange");
+			}else if(lennum > 20){
+				$("#inp_txt").addClass("orange");
+				$("#lengthnums").addClass("orange");
+				$(".sub_btn").removeClass("obs")
+			}else if(lennum == 0){
+				$(".sub_btn").removeClass("obs");
+				$("#inp_txt").removeClass("orange");
+				$("#lengthnums").removeClass("orange");
+			}
+		}
+		$(".sub_btn").on("click",function(){
+			if($("#lengthnums").text() > 20){
+				alert("注意字数限制")
+				return false;
+			}else if($("#lengthnums").text() == 0){
+				alert("备注不能为空")
+				return false;
+			}else{
+				console.log(666)
+			}
+		});
+	
+	
+	
+	$(".cancel").on("click",function(){
 		layer.closeAll();
 	})
 })
