8543da746a75a41caec7c458bfc12b1702b851a6
bo <bo>
2017-08-18 18:26:19 +0800

    8.18

---
+++ tuodaoWap/1/js/car/city.js
@@ -0,0 +1,366 @@
+var cityData = [
+	{
+		name: "A",
+		cities: [
+			{
+				name: "奥迪",
+			},
+			{
+				name: "阿斯顿·马丁",
+			},
+			{
+				name: "阿尔法·罗密欧",
+			}
+		]
+	},
+	{
+		name: "B",
+		cities: [
+			{
+				name: "奔驰",
+			},
+			{
+				name: "本田",
+			},
+			{
+				name: "宝骏",
+			},
+			{
+				name: "标致",
+			},
+			{
+				name: "保时捷",
+			},
+			{
+				name: "北汽绅宝",
+			},
+			{
+				name: "宾利",
+			},
+			{
+				name: "奔腾",
+			},
+			{
+				name: "北汽幻速",
+			},
+			{
+				name: "布加迪",
+			}
+		]
+	},
+	{
+		name: "C",
+		cities: [
+			{
+				name: "长安",
+			},
+			{
+				name: "长城",
+			},
+			{
+				name: "长安欧尚",
+			},
+			{
+				name: "昌河",
+			}
+		]
+	},
+	{
+		name: "D",
+		cities: [
+			{
+				name: "大众",
+			},
+			{
+				name: "东风风行",
+			},
+			{
+				name: "DS",
+			},
+			{
+				name: "东风风光",
+			},
+			{
+				name: "东南",
+			},
+			{
+				name: "道奇",
+			}
+		]
+	},
+	{
+		name: "F",
+		cities: [
+			{
+				name: "丰田",
+			},
+			{
+				name: "福特",
+			},
+			{
+				name: "法拉利",
+			},
+			{
+				name: "菲亚特",
+			}
+		]	
+	},
+	{
+		name: "G",
+		cities: [
+			{
+				name: "广汽传祺",
+			},
+			{
+				name: "观致",
+			},
+			{
+				name: "GMC",
+			}
+		]
+	},
+	{
+		name: "H",
+		cities: [
+			{
+				name: "哈弗",
+			},
+			{
+				name: "海马",
+			},
+			{
+				name: "红旗",
+			},
+			{
+				name: "悍马",
+
+			}
+		]
+	},
+	{
+		name: "J",
+		cities: [
+			{
+				name: "吉利汽车",
+
+			},
+			{
+				name: "Jeep",
+
+			},
+			{
+				name: "捷豹",
+
+			},
+			{
+				name: "江淮",
+
+			}
+		]
+	},
+	{
+		name: "K",
+		cities: [
+			{
+				name: "凯迪拉克",
+
+			},
+			{
+				name: "克莱斯勒",
+
+			}
+		]
+	},
+	{
+		name: "L",
+		cities: [
+			{
+				name: "雷克萨斯",
+
+			},
+			{
+				name: "路虎",
+
+			},
+			{
+				name: "铃木",
+
+			},
+			{
+				name: "林肯",
+
+			},
+			{
+				name: "雷诺",
+
+			},
+			{
+				name: "兰博基尼",
+
+			},
+			{
+				name: "劳斯莱斯",
+
+			},
+			{
+				name: "陆风",
+
+			}
+		]
+	},
+	{
+		name: "M",
+		cities: [
+			{
+				name: "马自达",
+
+			},
+			{
+				name: "玛莎拉蒂",
+
+			},
+			{
+				name: "名爵",
+
+			},
+			{
+				name: "MINI",
+
+			},
+			{
+				name: "迈凯伦",
+			},
+			{
+				name: "迈巴赫",
+			}
+		]
+	},
+	{
+		name: "N",
+		cities: [
+			{
+				name: "纳智捷",
+			}
+		]
+	},
+	{
+		name: "O",
+		cities: [
+			{
+				name: "讴歌",
+			}
+		]
+	},
+	{
+		name: "P",
+		cities: [
+			{
+				name: "帕加尼",
+			}
+		]
+	},
+	{
+		name: "Q",
+		cities: [
+			{
+				name: "起亚",
+			},
+			{
+				name: "奇瑞",
+			},
+			{
+				name: "启辰",
+			}
+		]
+	},
+	{
+		name: "R",
+		cities: [
+			{
+				name: "日产",
+			},
+			{
+				name: "荣威",
+			}
+		]
+	},
+	{
+		name: "S",
+		cities: [
+			{
+				name: "斯柯达",
+			},
+			{
+				name: "三菱",
+			},
+			{
+				name: "斯巴鲁",
+			},
+			{
+				name: "双龙",
+			},
+			{
+				name: "smart",
+			}
+		]
+	},
+	{
+		name: "T",
+		cities: [
+			{
+				name: "特斯拉",
+			}
+		]
+	},
+	{
+		name: "W",
+		cities: [
+			{
+				name: "沃尔沃",
+			},
+			{
+				name: "五菱汽车",
+			}
+		]
+	},
+	{
+		name: "X",
+		cities: [
+			{
+				name: "现代",
+			},
+			{
+				name: "雪佛兰",
+			},
+			{
+				name: "雪铁龙",
+			}
+		]
+	},
+	{
+		name: "Y",
+		cities: [
+			{
+				name: "英菲尼迪",
+			},
+			{
+				name: "一汽",
+			},
+			{
+				name: "野马汽车",
+			}
+		]
+	},
+	{
+		name: "Z",
+		cities: [
+			{
+				name: "众泰",
+			},
+			{
+				name: "中华",
+			}
+		]
+	}
+];
+
