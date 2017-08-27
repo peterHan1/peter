14d6d05fc5b0e05b2e811677370662cf74d57ef6
bo <bo>
2017-08-27 18:35:01 +0800

    8.27

--- tuodao1/src/page/invest_detail/index.js
+++ tuodao1/src/page/invest_detail/index.js
@@ -3,7 +3,9 @@ require('./invest_detail.js');
 require('page/common/top/index.js');
 require('page/common/nav/index.js');
 require('page/common/footer-nav/index.scss');
-
+require('util/placeholder.js');
+require('util/layer/layer.js');
+require('../../util/layer/layer.scss');
 require('./fancybox/jquery.lazyload.min.js');
 require('./fancybox/source/jquery.fancybox.js');
 require('./fancybox/source/jquery.fancybox.scss');
--- tuodao1/src/page/invest_detail/invest_detail.js
+++ tuodao1/src/page/invest_detail/invest_detail.js
@@ -22,11 +22,20 @@ $(function(){
 	$(".investting input").blur(function(){
 		$(this).parent().removeClass("com_sty");
 	});
+	// 余额全投
+	$(".all_money").on("click",function(){
+		var money = $(".moneys").html();
+		$(".sub_money").keyup();
+		$(".sub_money").val(money);
+		var inputs = $(".sub_money");
+		inp(inputs);
+
+	});
 	// 支付按钮点击的状态
 	$(".sub_btn").on("click",function(){
 		var money = $(".sub_money").val();
 		var psw = $(".sub_psw").val();
-		if(money == "" && psw == ""){
+	/*	if(money == "" && psw == ""){
 			show_mess("请填写加入金额! 和 请填写加入金额和支付密码!");
 		}else if(money == ""){
 			show_mess("请填写加入金额!");
@@ -39,17 +48,78 @@ $(function(){
 				$(".current_money").removeClass("cur_money");
 				$(".mess").remove();
 			}
+		} */
+		// 加入成功弹窗
+		layer.open({
+			type: 1,
+			title:'',
+			skin: 'succeed_show',
+			area:['560px','360px'],
+			content: $('#bank_show')
+		});
+	});
+	$(".iskonw,.rclose").on("click",function(){
+		layer.closeAll();
+	});
+
+	$(".ul_select li").on("click",function(){
+		var clas = $(this).attr("class");
+		var sel = '<b class="select_b"></b>';
+		if(clas != "yhq_no"){
+			var datas = $(this).attr('data');
+			$(".ul_select li .select_b").remove();
+			$(this).append(sel);
+			$(".yes").addClass("add_quan");
+			$(".yes").attr("data",datas);
+		}
+	});
+	$(document).on("click",".discount_bot .add_quan",function(){
+		var dat = $(this).attr("data");
+		$(".inp_ticket").val(dat);
+		$(this).removeClass("add_quan");
+		layer.closeAll();
+	});
+	$(".discount_bot .no").on("click",function(){
+		$(".ul_select li .select_b").remove();
+		$(".yes").removeClass("add_quan");
+		layer.closeAll();
+	});
+	// 优惠券点击
+	$(".ticket").on("click",function(){
+		var val = $(".inp_ticket").val();
+		var sel = '<b class="select_b"></b>';
+		if($(".sub_money").val() == ""){
+			var a = $(".sub_money").parent(".invest_money");
+			input_mess("选择优惠券前需要填写加入金额！",a);
+		}else{
+			$(".ul_select li").each(function(){
+				var vals = $(this).attr("data");
+				if(val == vals){
+					$(this).append(sel);
+				}
+			});
+			layer.open({
+				type: 1,
+				title:'',
+				skin: '',
+				area:['635px','485px'],
+				content: $('#discount_show')
+			});
 		}
 	});
 	// 输入金额input输入状态
 	$(".sub_money").keyup(function(){
-		var a=$(this);
+		var inputs = $(this);
+		inp(inputs);
+	});
+	function inp(ins){
+		var a=ins;
 		if(/[^\d]/.test(a.val())){
 			var temp_amount=a.val().replace(/[^\d.]/g,'');
-			$(this).val(temp_amount);
+			a.val(temp_amount);
 		};
 		// 最后根据算法优化
-		var b = $(this).val();
+		var b = a.val();
 		// 有奖励
 		var flag = false;
 		var c = 6;
@@ -65,20 +135,19 @@ $(function(){
 		}else{
 			$(".predict_money").html(b);
 		}
-		var m = $(this).val();
+		var m = a.val();
 		if(m == ""){
 			$(".sub_btn").val("实付0.00元，立即投资");
+			$(".predict_money").html("0.00");
 		}else{
 			$(".sub_btn").val("实付"+m+"元，立即投资");
 		}
-
-	});
+	};
 
 	// 输入金额input失去焦点状态
 	$(".sub_money").blur(function(){
 		var money = $(this).val();
 		var a = $(this).parent(".invest_money");
-		console.log(money);
 		if(money == 666){
 			input_mess("余额不足",a);
 		}else if(money == 500){
@@ -87,18 +156,15 @@ $(function(){
 			input_mess("单笔限额为500,000元！",a);
 		}else if(money == 123){
 			input_mess("您输入的金额大于当前剩余可投金额！",a);
+		}else if(money < 100){
+			input_mess("不得低于起投金额100元！",a);
 		}else{
 			a.removeClass("bor_col");
 			$(".in_span").remove();
 		}
+
 	});
-	// 优惠券点击
-	$(".ticket").on("click",function(){
-		if($(".sub_money").val() == ""){
-			var a = $(".sub_money").parent(".invest_money");
-			input_mess("选择优惠券前需要填写加入金额！",a);
-		};
-	});
+
 	// input状态提示
 	function input_mess(str,par){
 		if ($(".in_span").length>0) {
