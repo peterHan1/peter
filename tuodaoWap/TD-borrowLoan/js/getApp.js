a167dcf7d187f00aed351e254ad35d1334acefee
bo <perter_han@163.com>
2019-03-11 16:08:28 +0800

    3.11

--- tuodaoWap/TD-borrowLoan/js/getApp.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	//判断页面是否在app中打开,去除title TDJF暂定字符串
-	if (navigator.userAgent.indexOf("TDJF") != -1) {
-		$(".tilte").hide();
-		$(".banner").removeClass("isApp"); 
-		$(".borrowBox").removeClass("borrowBoxApp"); 
-		$(".borrowFrom").removeClass("isApp");
-	}
-})	
--- tuodaoWap/openBorrow/js/base.js
+++
@@ -1,8 +0,0 @@
-re_size();
-window.onresize = re_size;
-function re_size(){
-	var deviceWidth = document.documentElement.clientWidth;
-	if(deviceWidth > 414) deviceWidth = 414;
-	document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
-}
-
